7f62da3c2470a0316d5834e8f0add45584d781e0
Matt Martz <matt.martz@gmail.com>
2022-02-08 13:45:09 -0500

    add pr project

--- bin/blog-pipeline.ts
+++ bin/blog-pipeline.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { BlogPipelineStack } from '../lib/blog-pipeline-stack';
 import { SecretValue, Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
+import { BuildSpec, EventAction, FilterGroup, GitHubSourceCredentials, Project, Source } from 'aws-cdk-lib/aws-codebuild';
 import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
 
 const app = new cdk.App();
@@ -24,7 +24,7 @@ class BlogPipeline extends Stack {
     const branch = 'main';
     const secretArn = 'arn:aws:secretsmanager:us-east-1:359317520455:secret:BlogPipelineGitHubToken-gwKanq';
     const pipelineSpec = BuildSpec.fromObject({
-      version: '0.2',
+      version: 0.2,
       phases: {
         install: {
           commands: ['n latest', 'node -v', 'npm ci'],
@@ -51,6 +51,44 @@ class BlogPipeline extends Stack {
 
     const stage = new BlogPipelineStage(app, 'BlogPipelineStage', {});
     pipeline.addStage(stage);
+
+    new GitHubSourceCredentials(this, 'GitHubCreds', {
+      accessToken: SecretValue.secretsManager('arn:aws:secretsmanager:us-east-1:359317520455:secret:BlogPipelineGitHubToken-gwKanq', {
+        jsonField: 'access-token',
+      }),
+    });
+
+    const prSpec = BuildSpec.fromObject({
+      version: 0.2,
+      phases: {
+        install: {
+          commands: ['n latest', 'node -v', 'npm ci'],
+        },
+        build: {
+          commands: ['npm run test']
+        }
+      }
+    });
+
+    const source = Source.gitHub({
+      owner: owner,
+      repo: repo,
+      webhook: true,
+      webhookFilters: [
+        FilterGroup.inEventOf(
+          EventAction.PULL_REQUEST_CREATED,
+          EventAction.PULL_REQUEST_UPDATED,
+          EventAction.PULL_REQUEST_REOPENED,
+        ).andBranchIsNot('main'),
+      ],
+      reportBuildStatus: true,
+    });
+
+    new Project(this, 'PullRequestProject', {
+      source,
+      buildSpec: prSpec,
+      concurrentBuildLimit: 1,
+    });
   }
 }
 
